c6ca14b2b1dfb7c5ba2edeb45b220ad0be072240
ARACHNOlD <sumit11dhamala@gmail.com>
2024-05-11 12:05:41 +0545

    initial

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . models import Question, Answer
 
-# Register your models here.
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    max_num = 4
+    min_num = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+    list_display = ['title', 'created_at']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
--- app/views.py
+++ app/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Question
 
 # Create your views here.
 
 
 def home_view(request):
+    session_user_info = request.session.get("player_name", None)
+    if request.method == "POST":
+        user_name = request.POST["name"]
+        request.session["player_name"] = user_name
+        return redirect("question", question_id=1)
+    context = {
+        "player_name": session_user_info
+    }
     return render(request, "index.html")
 
 
 def question_view(request, question_id=None):
-    return render(request, "question.html")
+    questions = Question.objects.get(id=question_id)
+    context = {
+        "question": questions
+    }
+    return render(request, "question.html", context)
 
 
 def scoreboard_view(request):
